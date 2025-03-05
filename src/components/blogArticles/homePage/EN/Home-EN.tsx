c019c6a767035b1449e52e13a88433d471b2f558
Salim Thaifa <s.thaifa@amsterdam.nl>
2025-03-05 16:08:02 +0100

    fix: homepage texts in NL / EN

--- src/components/blogArticles/homePage/EN/Home-EN.tsx
+++ src/components/blogArticles/homePage/EN/Home-EN.tsx
@@ -1,4 +1,11 @@
-import { Blockquote, Grid, Spotlight } from '@amsterdam/design-system-react';
+import {
+  Accordion,
+  Blockquote,
+  Grid,
+  Link,
+  Paragraph,
+  Spotlight,
+} from '@amsterdam/design-system-react';
 import SwiperCube from '../../../animatedComponents/swiperCube/swiperCube';
 
 export default function Home() {
@@ -15,6 +22,59 @@ export default function Home() {
         </Grid>
       </Spotlight>
       <SwiperCube />
+
+      {/* Texts */}
+      <div className="accordion">
+        <Accordion headingLevel={1}>
+          <Accordion.Section label="Who am I?">
+            <Paragraph>
+              I am Salim Thaifa, a Fullstack Developer in the Tech department of
+              the Municipality of Amsterdam, within the Digitalization,
+              Innovation & Information (DII) cluster. I am passionate about
+              creativity, technology, and innovation. Originally, I am a civil
+              engineer, but four years ago, I made the switch to software
+              development/engineering because it suits me perfectly: it’s
+              fast-paced, creative, impressive, efficient, and you can
+              immediately see the results of your hard work. As a self-taught
+              programmer—YES, without using AI or ChatGPT—I mostly learned by
+              thinking for myself, starting projects I came up with, and using
+              platforms like Stack Overflow, YouTube, courses, and other forums.
+              I enjoy learning from experts and continuously work on improving
+              my skills. My interests are diverse: I love DIY projects and
+              always try to fix things when they break—my wife sometimes thinks
+              I’m crazy because I never stop until it’s actually fixed. It often
+              takes me a lot of time and energy, but it's a guilty pleasure!
+              Additionally, I enjoy solving mechanical and 3D puzzles,
+              appreciating art in general, cooking, and adrenaline-giving
+              activities. I believe in the power of open source and love sharing
+              my knowledge with others!
+            </Paragraph>
+          </Accordion.Section>
+          <Accordion.Section label="Why a Blog?">
+            <Paragraph>
+              I wanted to create a space to gather all my notes, experiences,
+              and thoughts from the conferences I’ve attended and share my
+              insights with others. With many more conferences planned in the
+              future, it’s nice to have everything in one place for easy
+              reference. Additionally, this blog gives me the perfect
+              opportunity to work with the thoughtfully designed Amsterdam
+              Design System— hats off to the talented team behind it! It’s been
+              a truly enjoyable experience to use it in this context.
+            </Paragraph>
+          </Accordion.Section>
+          <Accordion.Section label="The open source github repository">
+            <Paragraph>
+              <Link href="https://github.com/MSG41/st-dev-conf">
+                {' '}
+                This is the link to the open-source GitHub repository.
+              </Link>
+              {''} I know it's not perfect yet, and I will do my best to improve
+              it as much as I can. Any suggestions or feedback are always
+              welcome.
+            </Paragraph>
+          </Accordion.Section>
+        </Accordion>
+      </div>
     </div>
   );
 }
--- src/components/blogArticles/homePage/NL/Home-NL.tsx
+++ src/components/blogArticles/homePage/NL/Home-NL.tsx
@@ -1,4 +1,11 @@
-import { Blockquote, Grid, Spotlight } from '@amsterdam/design-system-react';
+import {
+  Accordion,
+  Blockquote,
+  Grid,
+  Link,
+  Paragraph,
+  Spotlight,
+} from '@amsterdam/design-system-react';
 import SwiperCube from '../../../animatedComponents/swiperCube/swiperCube';
 
 export default function Home() {
@@ -14,8 +21,55 @@ export default function Home() {
           </Grid.Cell>
         </Grid>
       </Spotlight>
-
       <SwiperCube />
+
+      {/* TEKSTEN */}
+      <div className="accordion">
+        <Accordion headingLevel={1}>
+          <Accordion.Section label="Wie ben ik?">
+            <Paragraph>
+              Ik ben Salim Thaifa, Fullstack Developer bij Gemeente Amsterdam,
+              cluster Digitalisering, Innovatie & Informatie (DII). Ik ben
+              gepassioneerd door creativiteit, technologie en innovatie.
+              Oorspronkelijk ben ik civiel ingenieur, maar vier jaar geleden
+              maakte ik de overstap naar softwareontwikkeling, omdat het perfect
+              bij me past: het is snel, creatief, indrukwekkend, efficiënt en je
+              ziet direct de resultaten van je harde werk. Als autodidact heb ik
+              mezelf leren programmeren — JA, zonder AI of ChatGPT — maar vooral
+              door zelf na te denken en projecten voor mezelf te starten, en
+              door gebruik te maken van platforms zoals Stack Overflow, YouTube,
+              cursussen en andere forums. Ik leer graag van experts en blijf
+              mezelf continu ontwikkelen. Mijn interesses zijn breed; ik hou
+              onder andere van klussen en probeer altijd dingen te repareren als
+              ze kapot zijn — mijn vrouw denkt soms dat ik gek ben! Verder hou
+              ik van denkpuzzels, kunst, eten en koken, en adrenaline-gevende
+              activiteiten. Ik geloof in de kracht van open source en deel mijn
+              kennis graag met anderen!
+            </Paragraph>
+          </Accordion.Section>
+          <Accordion.Section label="Waarom deze blog?">
+            <Paragraph>
+              Ik wilde een plek waar ik mijn ervaringen en inzichten van
+              conferenties kan verzamelen en delen. Het is fijn om alle
+              conferenties op één plek terug te kunnen vinden in een
+              persoonlijke omgeving. Daarnaast biedt deze blog me de perfecte
+              kans om te werken met het doordacht ontworpen Amsterdam Design
+              System — petje af voor het getalenteerde team achter dit systeem!
+              Het is een echt plezier om het in deze context te gebruiken.
+            </Paragraph>
+          </Accordion.Section>
+          <Accordion.Section label="De open-source GitHub-repository">
+            <Paragraph>
+              <Link href="https://github.com/MSG41/st-dev-conf">
+                {' '}
+                Dit is de link naar de open-source GitHub repository.
+              </Link>{' '}
+              Ik weet dat het nog niet perfect is en ik zal mijn best doen om
+              het te verbeteren. Alle suggesties en feedback zijn altijd welkom.
+            </Paragraph>
+          </Accordion.Section>
+        </Accordion>
+      </div>
     </div>
   );
 }
